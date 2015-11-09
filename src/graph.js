ec2a7c8ec753dceddb114450936c122c992beab1
Gal Tashma <gtappsdev@gmail.com>
2015-11-09 21:37:01 +0000

    added graph walker to help walk the graph

--- src/graph.js
+++ src/graph.js
@@ -17,6 +17,9 @@ DiGraph.prototype.addPath = function(path){
     this._refreshMeta();
 };
 
+DiGraph.prototype.getNodes = function(){
+    return this._nodes;  
+};
 
 DiGraph.prototype.getNode = function(id){
     return this._nodes[id];
@@ -26,7 +29,7 @@ DiGraph.prototype.getBeginnings = function(){
     var begginnings = [];
     for (var key in this._nodes){
         var node = this._nodes[key];
-        if (node.callCount === 0){
+        if (node.getCallCount() === 0){
             begginnings.push(node);
         }
     }
@@ -42,23 +45,17 @@ DiGraph.prototype.getRankSize = function(rankNum){
     return this._cacheRankCount[rankNum] || 0;
 };
 
-DiGraph.prototype.forEach = function(iteratorFunction, thisObj){
-    for (var key in this._nodes){
-        var node = this._nodes[key];
-        if (node !== undefined){
-            iteratorFunction.call(thisObj, node);    
-        }
-    }
-};
 
 DiGraph.prototype._refreshMeta = function(){
     this._longestPathLength = this._rankNodes();
     this._cacheRankCount = this._getRankCount();
+    this._organize();
 };
 
 DiGraph.prototype._addPath = function(path){
     var lastNode = undefined;
     
+    // Init add add next items
     path.forEach(function(id){
         if (this._nodes[id] === undefined){
             this._nodes[id] = new Node(id);
@@ -89,7 +86,7 @@ DiGraph.prototype._rankNodes = function(){
     
     // Copy the call count to a temporary map
     var tmpCallCount = {};
-    for (var key in this._nodes){ tmpCallCount[key] = this._nodes[key].callCount }
+    for (var key in this._nodes){ tmpCallCount[key] = this._nodes[key].getCallCount() }
     
     // An array that allways contains all the ids of the nodes that have a call count of 0 (at the time)
     var currentStage;
@@ -128,49 +125,126 @@ DiGraph.prototype._getRankCount = function(){
     return rankCount;
 };
 
+DiGraph.prototype._organize = function(){
+    
+    var that = this;
+    var walker = new GraphWalker(this);
+    
+    var tryShiftNodesForward = function(node){
+        var forefathers = walker.getForefathers(node);
+        if (forefathers.length === 0){
+            console.log("CAN DO!");
+            return true;
+            // can do!
+        }
+        return false;
+        
+    };
+    
+    var makeUpForLongLine = function(startRank, endRank){
+        console.log("making up for long line", startRank, endRank);
+        for(var i=startRank+1; i < endRank; i++){
+            var count = that._cacheRankCount[i] || 0;
+            console.log("index", i, "before", that._cacheRankCount[i]);
+            that._cacheRankCount[i] = count*2;
+            console.log("index", i, "count", that._cacheRankCount[i]);
+        }
+    };
+    
+    
+    walker.forEach(function(node){
+        
+        node.next.each(function(nextNodeId){
+            var nextNode = this.getNode(nextNodeId);
+            var distance = nextNode.rank - node.rank;
+            
+            if (distance > 1){
+                var successful = tryShiftNodesForward(node);
+                if (!successful){
+                    console.log("making up for nodes: ", node, nextNode);
+                    makeUpForLongLine(node.rank, nextNode.rank);
+                }
+            }
+        }, this);
+        
+    }, this);  
+};
+
+/* ---------------------- WALKER ----------------------*/
+
+function GraphWalker(graph){
+    this.graph = graph;
+}
+
+GraphWalker.prototype.forEach = function(iteratorFunction, thisObj){
+    var nodes = this.graph.getNodes();
+    for (var key in nodes){
+        var node = nodes[key];
+        if (node !== undefined){
+            iteratorFunction.call(thisObj, node);    
+        }
+    }
+};
+
+GraphWalker.prototype.iterNext = function(iteratorFunction, node, thisObj){
+    return this._iterNodeProperty(iteratorFunction, node, "next", thisObj);
+};
+
+GraphWalker.prototype.iterPrev = function(iteratorFunction, node, thisObj){
+    return this._iterNodeProperty(iteratorFunction, node, "callers", thisObj);
+};
+
+GraphWalker.prototype._iterNodeProperty = function(iteratorFunction, node, propertyName, thisObj){
+    var round = node[propertyName] || new Set();
+    
+    while (round.size() > 0){
+        
+        var nextRound = new Set();
+        round.each(function(nextRoundId){
+            var node = this.graph.getNode(nextRoundId);
+            iteratorFunction.call(thisObj, node);
+        }, this);
+        round = nextRound;
+    }
+};
 
 
+GraphWalker.prototype.getForefathers = function(node){
+    var forefathers = [];
+    this.iterPrev(function(childNode){
+        console.log("what is hiding here?", childNode);
+        if(childNode.getCallCount() === 0){
+            forefathers.push(childNode);
+        }
+    }, node, this);
+    
+    return forefathers;
+};
 
-// DiGraph.prototype._beautfiy = function(){
-//     var nodeRankMap = {}; // mapping #rank -> set(nodes...)
-    
-//     for(var id in this._nodes){
-//         var node = this._nodes[id];
-//         nodeRankMap[node.rank] = nodeRankMap[node.rank] || new Set();
-//         nodeRankMap[node.rank].add(node.id);
-//     }
-    
-//     // for eah node, if the min diff from the rank of the next node is bigger than one:
-//     // move that node farword 
-//     // go from the end to the beginning ?
-    
-//     for (var i in nodeRankMap){
-//         var nodeIds = nodeRankMap[i];
-//         nodeIds.each(function(id){
-//         }, this);
-//     }
-    
-// };
 
 /* ----------------------- NODE -----------------------*/
 
 
 function Node(id, data){
-    this.id = id;           // id
-    this.next = new Set();  // the ids of the next nodes
-    this.callCount = 0;     // number of times this node is refrenced by other nodes
-    this.rank = 0;          // longest distance from the beginning
-    //this.x = 0;             // if to be drawn on a chart, it would be the optimal x axis location 
+    this.id = id;               // id
+    this.next = new Set();      // the ids of the next nodes
+    this.callers = new Set();   // the ids of all the nodes that call this node
+    this.rank = 0;              // longest distance from the beginning
+    //this.x = 0;               // if to be drawn on a chart, it would be the optimal x axis location 
 }
 
-Node.prototype._incrCaller = function(){
-    this.callCount += 1;
+Node.prototype.getCallCount = function(){
+    return this.callers.size();
 };
 
 Node.prototype.addNext = function(node){
     if(this.next.add(node.id)){
-        node._incrCaller();    
+        node._addCaller(this);
     }
+};
+
+Node.prototype._addCaller = function(node){
+    this.callers.add(node.id);
 };
 
 
@@ -218,5 +292,11 @@ Set.prototype = {
 		        iteratorFunction.call(thisObj, this._values[hash]);    
 		    }
 		}
+	},
+	
+	append: function append(set){
+	    set.each(function(value){
+	        this.add(value);
+	    }, this);
 	}
 };
